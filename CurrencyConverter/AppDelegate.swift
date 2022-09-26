11e51f5e60e4941ee77400d7e7b2d50608078569
mahedeezaman <mahedeezaman@iut-dhaka.edu>
2022-09-26 06:38:54 +0600

    Removing Main storyboard and creating new UIViewController

--- CurrencyConverter/AppDelegate.swift
+++ CurrencyConverter/AppDelegate.swift
@@ -9,7 +9,13 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = CurrencyConverterMainViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
---
+++ CurrencyConverter/CurrencyConverterMainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyConverterMainViewViewController.swift
+//  CurrencyConverter
+//
+//  Created by Mahedee Zaman on 26/09/2022.
+//
+
+import UIKit
+
+class CurrencyConverterMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
