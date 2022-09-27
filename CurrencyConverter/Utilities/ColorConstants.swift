0ba8d108a27f31172b0cef4565606894fb7230c9
mahedeezaman <mahedeezaman@iut-dhaka.edu>
2022-09-27 06:41:53 +0600

    Creating UIColor from hex and also adding a background colour in the home view controller

---
+++ CurrencyConverter/Utilities/ColorConstants.swift
@@ -0,0 +1,12 @@
+//
+//  ColorConstants.swift
+//  CurrencyConverter
+//
+//  Created by Mahedee Zaman on 27/09/2022.
+//
+
+import UIKit
+
+struct ColorConstants {
+    static var homeViewBackground = UIColor(hexString: "#4D648D")
+}
