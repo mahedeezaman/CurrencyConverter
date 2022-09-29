b2105357b1fe06998588ed4353a2e9443e75ab1a
mahedeezaman <mahedeezaman@iut-dhaka.edu>
2022-09-29 09:43:53 +0600

    Creating submit button and also adding constraint and background to the whole view

--- CurrencyConverter/View/Convert Currency Views/ConvertCurrencyViewController.swift
+++ CurrencyConverter/View/Convert Currency Views/ConvertCurrencyViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 
 class ConvertCurrencyViewController: UIViewController {
+    @IBOutlet var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        containerView.backgroundColor = ColorConstants.homeViewContainersColor
+        containerView.layer.cornerRadius = Constants.cornerRadius
+        containerView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
 }
