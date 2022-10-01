3434da892f0a23b42e8fede546d92758251554de
mahedeezaman <mahedeezaman@iut-dhaka.edu>
2022-10-01 07:39:57 +0600

    Creating view model

---
+++ CurrencyConverter/ViewModel/CurrencyViewModel.swift
@@ -0,0 +1,53 @@
+//
+//  CurrencyViewModel.swift
+//  CurrencyConverter
+//
+//  Created by Mahedee Zaman on 01/10/2022.
+//
+
+import Foundation
+
+class CurrencyViewModel: ObservableObject {
+    @Published var currencyData = CurrencyDataModel() {
+        didSet {
+            if StringUtilities.convertStringToDouble(data: currencyData.fromAmount) > StringUtilities.convertStringToDouble(data: userData.accountBalances[currencyData.fromCurrency]) {
+                DispatchQueue.main.async {
+                    self.currencyData.fromAmount = oldValue.fromAmount
+                }
+            }
+        }
+    }
+    @Published var userData : UserDataModel
+    
+    var commissionManager : CommissionManager
+    var dataManager : DataManager
+    
+    init() {
+        let tempDataManager = DataManager()
+        let tempCommissionManager = CommissionManager()
+        
+        self.commissionManager = tempCommissionManager
+        self.dataManager = tempDataManager
+        self.userData = tempDataManager.getUserData()
+    }
+    
+    func sendConversionRequest() {
+        if Double(userData.accountBalances[currencyData.fromCurrency]!)! < Double(currencyData.fromAmount)! {
+            print(CurrencyConversionErrorModels.insufficientAmount.getRawValue())
+            return
+        }
+        commissionManager.checkTransactionValidity(requestedData: currencyData, availabilityData: userData) { commissionAmount, error in
+            if let error {
+                print(error.getRawValue())
+            } else if let commissionAmount {
+                Task {
+                    let afterConversion = await self.dataManager.convertCurrency(from: self.currencyData.fromCurrency, to: self.currencyData.toCurrency, of: StringUtilities.convertStringToDouble(data: self.currencyData.fromAmount), withCommission: commissionAmount)
+                    DispatchQueue.main.async {
+                        self.userData = afterConversion
+                        print(self.userData)
+                    }
+                }
+            }
+        }
+    }
+}
