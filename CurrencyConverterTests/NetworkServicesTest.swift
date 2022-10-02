91489c20d80b61e2c96ed1b934d6e402f8b1a5a0
mahedeezaman <mahedeezaman@iut-dhaka.edu>
2022-10-02 12:59:38 +0600

    Creating network service test file

---
+++ CurrencyConverterTests/NetworkServicesTest.swift
@@ -0,0 +1,59 @@
+//
+//  NetworkServicesTest.swift
+//  CurrencyConverterTests
+//
+//  Created by Mahedee Zaman on 02/10/2022.
+//
+
+import XCTest
+@testable import CurrencyConverter
+
+final class NetworkServicesTest : XCTest {
+    var sut = NetworkService()
+    
+    func getDataModel() -> CurrencyDataModel {
+        return CurrencyDataModel(fromAmount: "10", fromCurrency: "EUR", toAmount: "10", toCurrency: "USD")
+    }
+    
+    func test_network_call_to_convert_data() async {
+        do {
+            let response = try await sut.getConvertedData(requestData: getDataModel())
+            XCTAssertNotNil(response)
+        } catch {
+            XCTAssertThrowsError(ErrorModels.serverError)
+        }
+    }
+    
+    func test_network_call_with_no_amount() async {
+        do {
+            var data = getDataModel()
+            data.fromAmount = ""
+            let response = try await sut.getConvertedData(requestData: data)
+            XCTAssertNil(response)
+        } catch {
+            XCTAssertThrowsError(ErrorModels.serverError)
+        }
+    }
+    
+    func test_network_call_with_no_currency() async {
+        do {
+            var data = getDataModel()
+            data.fromCurrency = ""
+            let response = try await sut.getConvertedData(requestData: data)
+            XCTAssertNil(response)
+        } catch {
+            XCTAssertThrowsError(ErrorModels.serverError)
+        }
+    }
+    
+    func test_network_call_with_no_currencyTo() async {
+        do {
+            var data = getDataModel()
+            data.toCurrency = ""
+            let response = try await sut.getConvertedData(requestData: data)
+            XCTAssertNil(response)
+        } catch {
+            XCTAssertThrowsError(ErrorModels.serverError)
+        }
+    }
+}
---
+++ CurrencyConverterTests/StringUtilitiesTest.swift
@@ -0,0 +1,13 @@
+//
+//  StringUtilitiesTest.swift
+//  CurrencyConverterTests
+//
+//  Created by Mahedee Zaman on 02/10/2022.
+//
+
+import XCTest
+@testable import CurrencyConverter
+
+final class StringUtilitiesTest: XCTestCase {
+    
+}
